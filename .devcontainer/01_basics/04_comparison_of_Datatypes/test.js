a71d4eec1cb99ed7a597b4942f9e59d85c52505d
SrdxDan <srdxdan@gmail.com>
2024-06-11 23:06:47 +0000

    javascript

---
+++ .devcontainer/01_basics/04_comparison_of_Datatypes/test.js
@@ -0,0 +1,17 @@
+console.log("2">1)
+
+
+console.log(null>0)
+console.log(null==0)
+console.log(null<0)
+console.log(null<=0)
+console.log(null>=0)
+
+
+console.log(undefined==0)
+console.log(undefined>=0)
+console.log(undefined>0)
+
+
+//triple check(===) => checks the data type too
+console.log(2==="2")
