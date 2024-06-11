6789c9751203dde679587f2bb1d94a45186fbf0a
SrdxDan <srdxdan@gmail.com>
2024-06-11 22:38:24 +0000

    javascript

---
+++ .devcontainer/01_basics/03_datatype_conversion/test.js
@@ -0,0 +1,28 @@
+let score ="213m"
+let value = Number(score)
+console.log(typeof value);
+console.log(value);
+
+score ="213"
+value = Number(score)
+console.log(typeof value);
+console.log(value);
+
+let isLoggedin="dan"
+let boolLoggedin= Boolean( isLoggedin)
+console.log(boolLoggedin)
+
+//1 => true, 0=> false
+// " " => false
+// " something "=> true
+
+//    ********************** OPERATIONS*************************
+
+let str1="Hello "
+let str2= "DAN"
+let str3=str1+str2
+console.log(str3)
+
+let counter=190
+counter++
+console.log(counter)
