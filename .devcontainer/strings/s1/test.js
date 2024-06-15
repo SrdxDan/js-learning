1ccc4688c58fd3a78dd0a16580b4cd734b88848d
SrdxDan <srdxdan@gmail.com>
2024-06-15 09:33:34 +0000

    js

---
+++ .devcontainer/strings/s1/test.js
@@ -0,0 +1,14 @@
+const name="dan"
+const repocount=50
+console.log(`My name is ${name} & my repocounnt is ${repocount}`)
+
+const gameName = new String("SrdxDan")
+
+console.log(gameName[1])
+console.log(gameName.__proto__)
+
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(3))
+console.log(gameName.indexof(n))
+//console.log(gameName.())
